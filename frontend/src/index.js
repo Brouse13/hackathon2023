1f408c4fa71fa2a07553302f5801d745d5f973c1
Jesus Castillo <jcasben02@gmail.com>
2023-05-13 15:27:23 +0200

    [UPDATE] Changed frontend content

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,30 @@
 const imagen = document.getElementById("contenedor");
 const persona = document.getElementById("persona");
 imagen.addEventListener("click",(evento) => {
-
-    console.log("asdasds");
-    var clickX = evento.clientX //Posición absoluta de 100px a la derecha del extremo izquierdo del elemento.
-    var clickY = evento.clientY; //Posición absoluta de 50px hacia abajo del extremo superior del elemento.
+    
+    let clickX = evento.clientX //Posición absoluta de 100px a la derecha del extremo izquierdo del elemento.
+    let clickY = evento.clientY; //Posición absoluta de 50px hacia abajo del extremo superior del elemento.
     console.log(clickX);
     console.log(clickY);
     
-    persona.style.left = (clickX) - 64 + "px";
-    persona.style.top = (clickY) - 64 + "px";
+    moveCharacter(clickX,clickY);
 });
 
-function border(){
-    if(persona.style.left>imagen.clientWidth){
-        persona.style.left = imagen.clientWidth;
+function moveCharacter(x,y){
+    //Look if the character gets out of the map from the left
+    if(x <= 40) {
+        persona.style.left = 40 + "px";
+    } else if((x + 128) > imagen.clientWidth) {
+        persona.style.left = imagen.clientWidth - 128 + "px";
+    } else {
+        persona.style.left = (x - 64) + "px";
     }
-    
-    if(persona.style.top>imagen.clientHeight){
-        persona.style.top = imagen.clientHeight;
+
+    if((y <= 65)) {
+        persona.style.top = 65 + "px";
+    } else if((y + 128) > imagen.clientHeight){
+        persona.style.top = imgaen.clientHeight - 128 + "px";
+    } else {
+        persona.style.top = (y - 64) + "px";
     }
 }
