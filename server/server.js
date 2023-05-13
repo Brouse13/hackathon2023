8066b7daa229c81ea35d295210f4b4b835456310
Brouse_13 <unaiunai2004@gmail.com>
2023-05-13 15:52:36 +0200

    Added socket events

--- server/server.js
+++ server/server.js
@@ -6,10 +6,12 @@ const bodyParser = require('body-parser')
 const multer = require('multer')
 const upload = multer()
 
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.use(express.static('frontend/src'))
+app.use(express.static(__dirname + '/public/'))
 
 //Parse application/json
 app.use(bodyParser.json()); 
@@ -26,10 +28,24 @@ require('./routes/users')(app)
 require('./routes/location')(app)
 require('./routes/login')(app)
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected')
+    })
+});
+
+app.get('/test', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
 })
 
 io.on('connection', (socket) => {
-    console.log("Connection succeed")
-})
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
